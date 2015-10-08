ec004f62307f406ae2ed4cfabbe411bda7dc0fe7
Lee Goddard <lee@leegoddard.net>
2015-10-08 09:57:52 +0200

    tests

--- spec/mocha.js
+++ spec/mocha.js
@@ -71,51 +71,46 @@ describe("Basic", function() {
             i.length.should.be.equal(3);
         } );
 
-
+        lsys.options.rules.should.deep.equal( [
+            [ "F($s,$o)", "$s == $AS && $o == $R", "F($AS,$L)F($BS,$R)" ],
+            [ "F($s,$o)", "$s == $AS && $o == $L", "F($BS,$L)F($AS,$R)" ],
+            [ "F($s,$o)", "$s == $BS", "F($AS,$o)" ]
+        ]);
     });
 });
 
-// test( 'Constructed with new parametric args', function () {
-//     var lsys = new Lsys( defaultOptions );
-//     equal( typeof lsys, "object", "Construted Lsys object" );
 
-//     // NB MooTools, not native:
-//     equal( typeOf( lsys.options.rules ), 'array', 'Rules array' );
-//     lsys.options.rules.each( function ( i ) {
-//         equal( typeOf( i ), 'array', 'Rule cast' );
-//         equal( i.length, 3, 'rule tuple' );
-//     } );
+describe( 'Interploation', function () {
+    it('Interpolates variable', function (){
+        var x = new Lsys( defaultOptions );
+        x.interploateVars( '$AS' );
+        x.should.equal(2);
+    });
+} );
 
-//     deepEqual(
-//         lsys.options.rules, [
-//             [ "F($s,$o)", "$s == $AS && $o == $R", "F($AS,$L)F($BS,$R)" ],
-//             [ "F($s,$o)", "$s == $AS && $o == $L", "F($BS,$L)F($AS,$R)" ],
-//             [ "F($s,$o)", "$s == $BS", "F($AS,$o)" ]
-//         ],
-//         'Rules parsed'
-//     );
-// } );
+describe( 'string to re and arg name', function () {
+    var rv = new Lsys( defaultOptions )
+        .string2reAndArgNames( 'F(s,o)' );
+    typeof( rv ).should.be.instanceof(Array);
+    rv.length.should.equal(2);
+    typeof( rv[ 0 ] ).should.be.instanceof(RegExp);
+    var varWord = '([\\$\\w-]+)';
+    rv[ 0 ].toString().should.equal(
+        '/(F)\\(' + varWord + ',' + varWord + '\\)/g'
+    );
+    typeof( rv[1] ).should.be.instanceof(Array);
+    rv[1].should.be.deep.equal([ 's', 'o' ] );
+} );
 
-// test( 'Interploation', function () {
-//     equal(
-//         new Lsys( defaultOptions )
-//         .interploateVars( '$AS' ),
-//         2,
-//         'Interpolate variable'
-//     );
-// } );
-
-// test( 'string to re and arg name', function () {
-//     var rv = new Lsys( defaultOptions )
-//         .string2reAndArgNames( 'F(s,o)' );
-//     equal( typeOf( rv ), 'array', 'rv type' );
-//     equal( rv.length, 2, 'rv length' );
-//     equal( typeOf( rv[ 0 ] ), 'regexp', 'rv regexp' );
-//     var varWord = '([\\$\\w-]+)';
-//     equal( rv[ 0 ], '/(F)\\(' + varWord + ',' + varWord + '\\)/g', 'rv regexp' );
-//     equal( typeOf( rv[ 1 ] ), 'array', 'rv var names type' );
-//     deepEqual( rv[ 1 ], [ 's', 'o' ], 'rv var names value' );
-// } );
+describe( 'Variable parsing', function () {
+    var varOpts = defaultOptions;
+    varOpts.variables += "\n#define $Test -0.5";
+    var lsys = new Lsys( varOpts );
+    lsys.variables.$AS.should.equal( 2);
+    lsys.variables.$L.should.equal(-1);
+    lsys.variables.$W.should.equal(0.5);
+    lsys.variables.$Test.should.equal(-0.5);
+} );
 
 // test( 'Constructor with bad rules', function () {
 //     var badOptions = Object.clone( defaultOptions );
@@ -139,22 +134,6 @@ describe("Basic", function() {
 //             'Bad variable parse error thrown as hoped'
 //         );
 //     }
-// } );
-
-// test( 'Variable parsing', function () {
-//     var varOpts = Object.clone( defaultOptions );
-//     varOpts.variables += "\n#define $Test -0.5";
-//     var lsys = new Lsys( varOpts );
-//     equal( lsys.variables.$AS, 2, 'positive int' );
-//     equal( lsys.variables.$L, -1, 'negative int' );
-//     equal( lsys.variables.$W, 0.5, 'positive float' );
-//     equal( lsys.variables.$Test, -0.5, 'negative float' );
-// } );
-
-// test( 'Math routines', function () {
-//     var lsys = new Lsys( defaultOptions );
-//     equal( lsys.dsin( 1 ), 0.01745240643728351, 'sin' );
-//     equal( lsys.dcos( 1 ), 0.9998476951563913, 'cos' );
 // } );
 
 // // ## Generate content
