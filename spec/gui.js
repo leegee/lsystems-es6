055b92689d19a1d9726d9dfbd9fcfbe143629fa6
Lee Goddard <lee@leegoddard.net>
2015-10-21 11:09:28 +0200

    new

---
+++ spec/gui.js
@@ -0,0 +1,18 @@
+"use strict";
+var GUI    = require("../lib/GUI"),
+    Lsys   = require( "../lib/LsysParametric.2d.js" ),
+    should = require('chai').should();
+
+describe('GUI', function (){
+    it('Should import', function(){
+        should.exist(GUI);
+    });
+
+    var gui = new GUI( Lsys, {} );
+
+    console.log('----', gui);
+
+    it('Should instantiate', function (){
+        gui.should.exist();
+    });
+});
